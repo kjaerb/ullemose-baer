d5d35f10692ed76e358610bad1392eed95fdaa21
kjaerb <kjaerb@gmail.com>
2023-05-22 11:03:46 +0200

    fix: resend with api key

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,9 +1,6 @@
 import { Resend, SendEmailData } from "resend";
-import { CreateEmailOptions } from "resend/build/src/emails/interfaces";
 
-const { NEXT_PUBLIC_RESEND_API_KEY } = process.env;
-
-const resend = new Resend(NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY as string);
 
 const options = {
   from: "kjaerb@gmail.com",
