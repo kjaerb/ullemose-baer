2df19ca21e94f5063065f51d4ddee199f3b4a227
kjaerb <kjaerb@gmail.com>
2023-06-02 15:21:13 +0200

    feat: added post in /api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,3 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ data: "OK" }, { status: 200 });
 }
+
+export async function POST() {
+  return NextResponse.json({ data: "OK" }, { status: 200 });
+}
