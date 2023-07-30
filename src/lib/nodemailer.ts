e98a1866d3cb463f5ed4a2d217b905a66ac556fe
kjaerb <kjaerb@gmail.com>
2023-07-30 15:29:45 +0200

    fix: remove pool

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -2,9 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 25,
+  port: 587,
   secure: false, // true for 465, false for other ports
-  pool: true,
   auth: {
     user: process.env.NEXT_PUBLIC_EMAIL,
     pass: process.env.EMAIL_PASSWORD,
